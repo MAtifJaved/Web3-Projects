28ccdc5562e45324a310b47390124f565c45dd94
MAtifJaved <atif_javed1992@yahoo.com>
2023-06-24 18:19:13 +0500

    Added Loader Functionality

--- Panaverse Website/panaverse-web/src/app/layout.tsx
+++ Panaverse Website/panaverse-web/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import ChakraWrapper from "@/components/ChakraWrapper";
+import { Suspense } from "react";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import Loading from "./loading";
 
 
 export default function RootLayout({
@@ -17,9 +19,11 @@ export default function RootLayout({
       <head />
       <body>
         <ChakraWrapper>
+          <Suspense fallback={<Loading />}>
           <Navbar/>
           {children}
           <Footer/>
+          </Suspense>
         </ChakraWrapper>
       </body>
     </html>
